808f9ba9f4c30155438c4e562cd9b122d1407646
Md Rakibul Islam <shawonnstu@gmail.com>
2024-12-11 01:02:26 +0100

    Just added some Menu

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    cout <<" This a project for a store for management purpose"<<endl;
+void displayMenu(){
+    cout << "Inventory Management System\n";
+    cout << "1. Please Add Product\n";
+    cout << "2. Update Product Stock\n";
+    cout << "3. Process Sale\n";
+}
 
+int main(){
+    //cout <<" This a project for a store for management purpose"<<endl;
+    displayMenu();
     return 0;
 }
