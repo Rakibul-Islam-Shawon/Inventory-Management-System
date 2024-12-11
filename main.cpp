b6182151a6cb7a65b9260ad63356a6b9b7995657
Md Rakibul Islam <shawonnstu@gmail.com>
2024-12-11 15:29:29 +0100

    Switch() added for product entry

--- main.cpp
+++ main.cpp
@@ -6,10 +6,64 @@ void displayMenu(){
     cout << "1. Please Add Product\n";
     cout << "2. Update Product Stock\n";
     cout << "3. Process Sale\n";
+    cout << "4. Display Stock\n";
+    cout << "5. Save Inventory to File\n";
+    cout << "6. Load Inventory from File\n";
+    cout << "7. Exit\n";
 }
 
 int main(){
     //cout <<" This a project for a store for management purpose"<<endl;
+    
+    int option, stock;
+    string id, name;
+    double price;
+    
     displayMenu();
+
+    while (true) {
+        displayMenu();
+        cout << "Enter your choice: ";
+        cin >> option;
+
+        switch (option) {
+        case 1:
+            cout << "Enter Product ID: ";
+            cin >> id;
+            cout << "Enter Product Name: ";
+            cin.ignore();
+            getline(cin, name);
+            cout << "Enter Stock Quantity: ";
+            cin >> stock;
+            cout << "Enter Product Price: ";
+            cin >> price;
+            //inventory.addProduct(Product(id, name, stock, price));
+            break;
+
+        case 2:
+            
+            break;
+
+        case 3:
+            
+            break;
+
+        case 4:
+           
+            break;
+
+        case 5:
+            
+            break;
+
+        case 6:
+            
+
+        case 7:
+            
+
+        default:
+            cout << "Invalid choice! Try again.\n";
+        }
     return 0;
 }
